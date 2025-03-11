2a2485bd222bd25c46731022b74a9db5930a32ca
Joe19071991 <lauranove66@gmail.com>
2025-03-11 16:06:50 +0100

    ajout d'identification

---
+++ eco_tech_front/src/Identification.jsx
@@ -0,0 +1,36 @@
+import "./style/Inscription.css";
+
+function Identification() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    console.log({ email, password });
+
+    form.reset();
+  };
+
+  return (
+    <>
+      <h2>Connectez-vous à votre compte</h2>
+      <div className="Identification">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Email</label>
+          <input name="email" type="email" id="email" required />
+
+          <label htmlFor="password">Mot de passe :</label>
+          <input type="password" id="password" name="password" required />
+
+          <button type="submit">Connection</button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default Identification;
--- eco_tech_front/src/main.jsx
+++ eco_tech_front/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Inscription from "./Inscription.jsx";
+import Identification from "./Identification.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Inscription />
+    <Identification />
   </StrictMode>
 );
