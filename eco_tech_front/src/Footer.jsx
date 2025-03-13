0f5047c3ffb3d4fa3ec4caf1b03001d7cccebe6d
leyanelouiset <louisetleyane@gmail.com>
2025-03-13 10:48:58 +0100

    footer part2

--- eco_tech_front/src/Footer.jsx
+++ eco_tech_front/src/Footer.jsx
@@ -1,28 +1,43 @@
 import React from 'react';
 import './Footer.css'; 
-
-
-
-
-
-
+import './fanta.css';
+import facebookLogo from './img.jpg/logo-facebook_1080029-107.webp';
+import  twitterLogo from'./img.jpg/twitter.webp';
+import instagramLogo from'./img.jpg/Instagram.webp';
+import linkedinLogo from './img.jpg/logo-linkedin.webp';
 function Footer() {
   return (
     <footer className="footer">
+      
    <div className="footer-container">
         <div className="footer-branding">
-          <h2>Mon Entreprise</h2>
-          <p>© {new Date().getFullYear()} Tous droits réservés</p>
+          <h4>Eco tech</h4>
+          {/* <image src {} alt='ecotech' */}
+          <p> {new Date().getFullYear()} consommer de façon responsable</p>
         </div>
 
+
+
+
+
         <div className="footer-social">
+
           <h3>Suivez-nous</h3>
+
           <div className="social-icons">
-            <a href="#" aria-label="Facebook" className="social-icon">FB</a>
-            <a href="#" aria-label="Twitter" className="social-icon">TW</a>
-            <a href="#" aria-label="Instagram" className="social-icon">IG</a>
-            <a href="#" aria-label="wattsap" className="social-icon">wattsap</a>
+            <a href="#" aria-label="Facebook" className="social-icon">
+            <img src={facebookLogo} alt="Facebook" />
+            </a>
+            <a href="#" aria-label="Twitter" className="social-icon">
+            <img src={twitterLogo } alt="twitter" />
+            </a>
+            <a href="#" aria-label="Instagram" className="social-icon">
+            <img src={instagramLogo} alt="instagram" />
+            </a>
+            <a href="#" aria-label="Linkedin" className="social-icon">   
+               <img src={linkedinLogo} alt="linkedin" /></a>
           </div>
+          
         </div>
       </div>
     </footer>
--- eco_tech_front/src/main.jsx
+++ eco_tech_front/src/main.jsx
@@ -1,6 +1,5 @@
-import Footer from './Footer.jsx'
- import { StrictMode } from "react";
-
+import { StrictMode } from "react";
+import Footer from "./footer.jsx";
 
  import { createRoot } from "react-dom/client";
  createRoot(document.getElementById("root")).render(
