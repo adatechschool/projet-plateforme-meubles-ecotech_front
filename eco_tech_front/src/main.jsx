bf802a203c0a037a2e3a443b7f2f5ae6c37667e5
Daniel <dvnielcueto@outlook.com>
2025-03-13 12:13:58 +0100

    git merge

--- eco_tech_front/src/main.jsx
+++ eco_tech_front/src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import Navbar from "./components/Header/Nav_bar.jsx";
+import Card from "./components/Card/data.jsx";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Navbar />
+      <Card />
     </BrowserRouter>
   </StrictMode>
 );
