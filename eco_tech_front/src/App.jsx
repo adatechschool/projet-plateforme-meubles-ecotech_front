9d17e55c1e5611f98f52591749dcb6f2cccf8327
leyanelouiset <louisetleyane@gmail.com>
2025-03-14 10:27:46 +0100

    new page smartphone

--- eco_tech_front/src/App.jsx
+++ eco_tech_front/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-
+import smartphone from "./style/Smartphone.jsx";
 import LoginPage from "./login/loginPage.jsx";
 function App() {
   return (
