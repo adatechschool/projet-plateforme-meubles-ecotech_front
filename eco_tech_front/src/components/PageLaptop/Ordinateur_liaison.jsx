13e5e49943c091743608b31ecb5dad6dd8a9a009
AssiarDev <halibaraissa@gmail.com>
2025-03-18 15:26:22 +0100

    update laptop

--- eco_tech_front/src/components/PageLaptop/Ordinateur_liaison.jsx
+++ eco_tech_front/src/components/PageLaptop/Ordinateur_liaison.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Products } from "../Card/Card";
 
-function laptop (){
+function Laptop (){
     const [products,setProducts] = useState([]) 
 
     useEffect(()=>{
