9dc8c0869d93975b7626d675cd8f1bb2a4eccc0a
Daniel <dvnielcueto@outlook.com>
2025-03-18 15:17:03 +0100

    file added for is admin

---
+++ eco_tech_front/src/login/IsAdmin.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+function IsAdmin () {
+    // if (!data.connected) {
+    //     console.log('Mot de passe incorrecte ');
+        
+    //     //return <Link to="/login"></Link>
+    // }
+    return <Link to="/"></Link>
+}
+
+export default IsAdmin;
