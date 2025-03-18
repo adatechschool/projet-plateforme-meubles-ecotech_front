922d6a6beabc65f6bdf14de832b7e8c307140d86
RoddickMhg <mahoungouroddick@gmail.com>
2025-03-18 14:19:20 +0100

    Route Moniteur debug

--- eco_tech_front/src/App.jsx
+++ eco_tech_front/src/App.jsx
@@ -6,6 +6,7 @@ import ListAllProducts from './components/Card/ListAllProducts.jsx'
 import Smartphon_liaison from "./components/PageSmartphone/Smartphon_liaison.jsx";
 import Ordinateur_liaison from "./components/PageLaptop/Ordinateur_liaison.jsx";
 import ProductDetails from "./components/productDetails.jsx"
+import MoniteurRoute from "./components/PageEcran/MoniteurRoute.jsx"
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/smartphone" element={<Smartphon_liaison />} />
         <Route path="/ordinateur" element={<Ordinateur_liaison />} />
         <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/moniteur" element={<MoniteurRoute />} />
       </Routes>
     </>
   );
